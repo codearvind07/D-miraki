fe3c5674b91d9af18e2755e3da204afb70ca277b
cpnsing <cpnsingh8181@gmail.com>
2025-05-22 06:33:11 +0530

    analyatics added

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-      
+      <Analytics/>
       <App />
     </ThemeProvider>
   </React.StrictMode>
