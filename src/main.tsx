4ed8f9b77736d839079b71c9530fbfd914ca54e9
cpnsing <cpnsingh8181@gmail.com>
2025-05-22 07:04:40 +0530

    helmet is added

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import App from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import "./index.css";
 import { Analytics } from "@vercel/analytics/react"
-
+import {  HelmetProvider } from 'react-helmet-async';
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <HelmetProvider>
+
     <ThemeProvider>
       <Analytics/>
       <App />
     </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
