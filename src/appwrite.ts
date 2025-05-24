b5261d890c9c0e0e9164b2875ce574ef47902204
cpnsing <cpnsingh8181@gmail.com>
2025-05-24 18:01:28 +0530

    blpg page added

--- src/appwrite.ts
+++ src/appwrite.ts
@@ -1,4 +1,4 @@
-import { Client, Databases, Storage, ID } from 'appwrite'
+import { Client, Databases, Storage } from 'appwrite'
 
 const client = new Client()
   .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite Endpoint
