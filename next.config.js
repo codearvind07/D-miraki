28d1ac9815fdb71f1d7027408e4c5962f2684ccf
cpnsing <cpnsingh8181@gmail.com>
2025-05-28 18:54:52 +0530

    next.config.mjs added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  distDir: 'dist',
+  images: { unoptimized: true },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
