b009791bf6e39c063d51b509aaf4583ef7cf5648
cpnsing <cpnsingh8181@gmail.com>
2025-05-30 08:41:47 +0530

    google tag manager added

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+   const GTM_ID = 'GTM-WSK6FWS8';
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
@@ -136,6 +137,15 @@ export default function RootLayout({
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `}
         </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `,
+        }} />
       </head>
       <body
         className={cn("min-h-screen w-full font-sans antialiased overflow-x-hidden", inter.variable)}
@@ -166,6 +176,10 @@ export default function RootLayout({
             })();
           `}
         </Script>
+        
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WSK6FWS8"
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
+
       </body>
     </html>
   );
